b3c2100331329c373c241d1a5cb01992081fa7f2
Brandon Denney <ee.brandon@gmail.com>
2023-12-07 06:38:50 -0500

    day 6 using MATH

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,24 +1,23 @@
+import java.lang.Math.pow
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 fun main() {
     fun part01(input: List<String>) {
         val records = parse(input)
         var marginOfError = 1
+        
         records.forEach { record ->
-            val raceTime = record.first
-            val distanceToBeat = record.second
-            var wins = 0
-            for (holdTime in 0..raceTime) {
-                val travelDistance = (raceTime - holdTime) * holdTime
-                if (travelDistance > distanceToBeat) {
-                    wins++
-                }
-            }
-            marginOfError *= wins
+            marginOfError *= marginForEntry(record)
         }
         println(marginOfError)
     }
     
     fun part02(input: List<String>) {
-        
+        val record = parsePart2(input)
+        println(marginForEntry(record))
     }
     
     val input = readInput("Day06Input")
@@ -32,6 +31,21 @@ fun main() {
     part02(input)
 }
 
+fun marginForEntry(entry: Pair<Long, Long>): Int {
+    val raceTime = entry.first
+    val distanceToBeat = entry.second
+
+    var topRange = (raceTime + sqrt(pow(raceTime.toDouble(), 2.0) - 4*(distanceToBeat))) / 2
+    var bottomRange = (raceTime - sqrt(pow(raceTime.toDouble(), 2.0) - 4*(distanceToBeat))) / 2
+
+    topRange = if (floor(topRange) == topRange) topRange - 1 else floor(topRange)
+    bottomRange = if (ceil(bottomRange) == bottomRange) bottomRange + 1 else ceil(bottomRange)
+
+    val winningTimeRange = Pair(bottomRange.toInt(), topRange.toInt())
+
+    return (topRange - bottomRange + 1).toInt()
+}
+
 fun parse(input: List<String>): List<Pair<Long, Long>> {
     val times = mutableListOf<Long>()
     times.addAll(input[0].split(":")[1].split(" ").filter { it.isNotBlank() }.map { it.toLong() })
@@ -40,4 +54,12 @@ fun parse(input: List<String>): List<Pair<Long, Long>> {
     distances.addAll(input[1].split(":")[1].split(" ").filter { it.isNotBlank() }.map { it.toLong() })
     
     return times.zip(distances)
+}
+
+fun parsePart2(input: List<String>): Pair<Long, Long> {
+    
+    val time = input[0].split(":")[1].split(" ").filter { it.isNotBlank() }.joinToString("").toLong()
+    val distance = input[1].split(":")[1].split(" ").filter { it.isNotBlank() }.joinToString("").toLong()
+
+    return Pair(time, distance)
 }
